78dc20391b5ec30e9fac88442d0e1b6457b891fa
Miguel Ceja <mcejap@gmail.com>
2018-04-28 11:40:52 -0500

    Setup editor to change title/body

---
+++ imports/ui/Editor.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { createContainer } from 'meteor/react-meteor-data'
+import { Session } from 'meteor/session'
+import PropTypes from 'prop-types';
+import {Meteor} from 'meteor/meteor'
+
+
+import { Notes } from '../api/notes'
+
+export class Editor extends React.Component{
+    handleBodyChange(e) {
+        this.props.call('notes.update', this.props.note._id, {
+            body: e.target.value
+        })
+    }
+    handleTitleChange(e){
+        this.props.call('notes.update', this.props.note._id, {
+            title: e.target.value
+        })
+    }
+    render(){
+        
+        if(this.props.note){
+            return (
+                <div>
+                    <input value={this.props.note.title} placeholder="Titulo perrón" onChange={this.handleTitleChange.bind(this)}/>
+                    <textarea value={this.props.note.body} placeholder="Your note here" onChange={this.handleBodyChange.bind(this)}></textarea>
+                    <button>Exterminar nota</button>
+                </div>
+            )
+        } else {
+            return(
+                <p>
+                    {this.props.selectedNoteId ? 'Sabe donde está tu nota' : 'Pick or create a note to get started'}
+                </p>
+            )
+        }
+    }
+}
+
+Editor.propTypes = {
+    note: PropTypes.object,
+    selectedNoteId: PropTypes.string
+}
+
+export default createContainer(() => {
+    const selectedNoteId = Session.get('selectedNoteId')
+
+    return {
+        selectedNoteId,
+        note: Notes.findOne(selectedNoteId),
+        call: Meteor.call
+    }
+}, Editor)
